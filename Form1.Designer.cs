381ce4e6e5f71507ca68b93171a6d44abc45c402
kaanc <kaancaydin3@gmail.com>
2025-05-31 11:45:38 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace odevB
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(744, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Sitka Subheading", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(966, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sayı Tahmin Oyunu";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 346);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(219, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Oyuna hoş geldiniz!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(14, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(575, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "1-100 arasındaki sayıyı tahmin edin. Deneme sayınız sayaçta gözükecektir.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(26, 341);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 27);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Tahmininiz:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(197, 265);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 45);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(25, 274);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(166, 27);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Deneme Sayınız:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(438, 305);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Century", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button2.Location = new System.Drawing.Point(381, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 80);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Sonuç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(14, 124);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(252, 23);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Başlat tuşuna basarak başlayın.";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label9.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label9.Font = new System.Drawing.Font("Sitka Subheading", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label9.Location = new System.Drawing.Point(0, 501);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(966, 37);
+            this.label9.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(966, 538);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Sayı Tahmin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odevB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sayac = 0;
+        int sayi;
+        int sifir = 0;
+        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            Random random = new Random();
+            sayi = random.Next(1, 101);
+            label7.Text = sayi.ToString();
+            MessageBox.Show("Bilgisayar sayısını tuttu. Tahmin etmek için yazı kısmına yazın.", "Başla!");
+            label9.Text = "";
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int tahmin = Convert.ToInt32(textBox1.Text);
+
+
+
+            if (tahmin == sayi)
+            {
+                MessageBox.Show("Tebrikler, bildiniz!","Doğru!");
+                label5.Text = sayac.ToString();
+                button2.Enabled = false;
+                textBox1.Clear();
+                label5.Text = sifir.ToString();
+                label9.Text = sayac.ToString() + " denemede bildiniz";
+                
+            }
+
+            if (tahmin > sayi)
+            {
+                sayac++;
+                label5.Text = sayac.ToString();
+                MessageBox.Show("tahmininiz sayıdan büyüktür. düşük sayılar deneyin.", "Yanlış!");
+            }
+
+            if (tahmin < sayi)
+            {
+                sayac++;
+                label5.Text = sayac.ToString();
+                MessageBox.Show("tahmininiz sayıdan küçüktür. büyük sayılar deneyin.","Yanlış!");
+            }
+
+            if (tahmin > 101)
+            {
+                
+                MessageBox.Show("ups, galiba bir yanlışlık oldu. lütfen 100ün altında değer girin.", "Hata!");
+
+            }
+
+            if (tahmin < 1)
+            {
+
+                MessageBox.Show("ups, galiba bir yanlışlık oldu. lütfen 1in üstünde değer girin.", "Hata!");
+
+            }
+
+        }
+    }
+}
